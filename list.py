a82e8115512c37acae4653cbd34f8db2f13e9270
infomail4575 <infomail4575@gmail.com>
2022-02-10 16:29:51 +0545

    list in python

---
+++ list.py
@@ -0,0 +1,30 @@
+# # python collection
+# list
+# tuple
+# set
+# dict
+
+# list
+# -Contain Mupltiple data
+# -Duplicate value
+# -Indexing
+# -Ordered
+# -Mutable
+
+a = [1,2,3,4,5,6,7,8,9]
+b = ["Apple","Ball","Cat","Dog"]
+print(type(a))
+print(b)
+
+b = ["Apple","Ball","Cat","Dog","Apple"]
+print(b)
+print(b*2)
+
+b = ["Apple","Ball","Cat","Dog","Apple"]
+b[1]
+
+b = ["Apple","Ball","Cat","Dog","Apple"]
+b[0:30]
+
+b = ["Apple","Ball","Cat","Dog","Apple"]
+b[0:4:2]
